0b6a1c1d8e93ae8204d534a3a3b3d10ba3b9857b
Jeremy Gibson <jgibson@caktusgroup.com>
2019-07-25 15:55:51 -0400

    Adds docker setup and local db setup

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,6 +13,17 @@ SECRET_KEY = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gardentracker',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '54328',
+    }
+}
+
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
