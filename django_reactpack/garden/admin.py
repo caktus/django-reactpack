9ff61d5fa1172c09b00daddc855ca3e1353815a3
Jeremy Gibson <jgibson@caktusgroup.com>
2019-07-26 11:32:30 -0400

    Wires up the admin

---
+++ django_reactpack/garden/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django_reactpack.garden.models import Bed, Garden, Plant
+
+
+admin.site.register(Garden)
+admin.site.register(Bed)
+admin.site.register(Plant)
+
